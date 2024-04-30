2ef77f7bae8d74276a44c1fe43abb075dcec7a83
Prakhar Verma <connect@iprakharv.com>
2024-04-30 17:30:30 -0400

    Created transaction.dart in models

---
+++ lib/models/transaction.dart
@@ -0,0 +1,22 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String(),
+    };
+  }
+}
